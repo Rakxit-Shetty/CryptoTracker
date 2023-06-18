a91a3ad3c13a703446365270d5c78d4f8da070a6
Rakxit-Shetty <rakshitshetty136@gmail.com>
2023-06-18 20:49:01 +0530

    footer update

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 //package
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //css
 import "./App.css";
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 //componets
 import Header from "../src/componets/Header";
 import HomePage from "../src/pages/HomePage";
 import CoinPage from "./pages/CoinPage";
 
-
 //style
-const useStyles = makeStyles(()=> ({
+const useStyles = makeStyles(() => ({
   App: {
-    backgroundColor: '#14161a',
+    backgroundColor: "#14161a",
     color: "white",
-    minHeight:"100vh"
-
+    minHeight: "100vh",
   },
 }));
 
 function App() {
-  const cls = useStyles()
+  const cls = useStyles();
   return (
     <BrowserRouter>
       <div className={cls.App}>
         <Header />
         <Routes>
-          <Route path="/" Component={HomePage} exact/>
+          <Route path="/" Component={HomePage} exact />
           <Route path="/coins/:id" Component={CoinPage} />
         </Routes>
       </div>
+      <footer className="footer">
+        <div>
+          &#169; {new Date().getFullYear()} to Learn React JS components by
+          <a href="https://rakshithshetty.vercel.app"> &nbsp; Rakxit-Shetty</a>
+        </div>
+      </footer>
     </BrowserRouter>
   );
 }
